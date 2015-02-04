d256376b5ddf2955bfeb870f9ce2e880e96b43e4
wangjing <wangjing@dhcp-140-42.nay.redhat.com>
2015-02-04 14:43:41 +0800

    re_url

--- tcms/testplans/urls/plan_urls.py
+++ tcms/testplans/urls/plan_urls.py
@@ -2,15 +2,33 @@
 
 from django.conf.urls import include, url, patterns
 
+from tcms.testplans.views import AddCaseToRunView
+from tcms.testplans.views import EditPlanView
+from tcms.testplans.views import AttachmentView
+from tcms.testplans.views import TestHistoryView
+
 urlpatterns = patterns('tcms.testplans.views',
     url(r'^(?P<plan_id>\d+)/$', 'get', name='test_plan_url'),
     url(r'^(?P<plan_id>\d+)/(?P<slug>[-\w\d]+)$', 'get', name='test_plan_url'),
-    url(r'^(?P<plan_id>\d+)/delete/$', 'delete'),
-    url(r'^(?P<plan_id>\d+)/chooseruns/$', 'choose_run'),
-    url(r'^(?P<plan_id>\d+)/edit/$', 'edit'),
-    url(r'^(?P<plan_id>\d+)/attachment/$', 'attachment'),
-    url(r'^(?P<plan_id>\d+)/history/$', 'text_history'),
-    url(r'^(?P<plan_id>\d+)/cases/$', 'cases'),
+    url(r'^(?P<plan_id>\d+)/chooseruns/$',
+        AddCaseToRunView.as_view(),
+        name='add_case_to_run'
+    ),
+    url(r'^(?P<plan_id>\d+)/edit/$',
+        EditPlanView.as_view(),
+        name='edit_plan_view'
+    ),
+    url(r'^(?P<plan_id>\d+)/attachment/$',
+        AttachmentView.as_view(),
+        name='attachment_view'
+    ),
+    url(r'^(?P<plan_id>\d+)/history/$',
+        TestHistoryView.as_view(),
+        name='testplan_text_history'
+    ),
+    url(r'^(?P<plan_id>\d+)/cases/$',
+        'cases'
+    ),
 )
 
 urlpatterns += patterns('tcms.testruns.views',
