08e9a7384c0a7b2c013e5b840d7c7f4186646280
wangjing <wangjing@dhcp-140-42.nay.redhat.com>
2015-02-06 09:45:39 +0800

    re_testplan_url

--- tcms/testplans/urls/plans_urls.py
+++ tcms/testplans/urls/plans_urls.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url, patterns
+from django.conf.urls import include, url, patterns
+
+from tcms.testplans.views import NewTestPlanView
+from tcms.testplans.views import ShowAllPlanView
+from tcms.testplans.views import CloneTestplanView
 
 urlpatterns = patterns('tcms.testplans.views',
-                       url(r'^$', 'all'),
-                       url(r'^new/$', 'new'),
-                       url(r'^ajax/$', 'ajax_search'),
-                       url(r'^treeview/$', 'tree_view'),
-                       url(r'^clone/$', 'clone'),
-                       url(r'^printable/$', 'printable'),
-                       url(r'^export/$', 'export'),
-                       url(r'^component/$', 'component'),
-                       )
+    url(r'^$',
+        ShowAllPlanView.as_view(),
+        name='show_all_plan_view'),
+    url(r'^new/$',
+        NewTestPlanView.as_view(),
+        name='testplan_new_test_plan'
+    ),
+    url(r'^ajax/$', 'ajax_search'),
+    url(r'^treeview/$', 'tree_view'),
+    url(r'^clone/$',
+        CloneTestplanView.as_view(),
+        name='testplans_clone_testplan'
+    ),
+    url(r'^printable/$','printable'),
+    url(r'^export/$', 'export'),
+    url(r'^component/$', 'component'),
+)
