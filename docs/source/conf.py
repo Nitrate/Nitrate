kiwitcms/Kiwi

Use version from module, not txt file
this is cleaner since we don't have to read .txt files and don't
modify the source before building a package.

This makes it possible to show the actual version in Nitrate's webUI
footer by exporting the version as a setting.

The setting can be overriden if desired, for example with
commands.getstatusoutput('rpm -q nitrate')[1]

Signed-off-by: Mr. Senko <atodorov@mrsenko.com>

docs/source/conf.py
@@ -21,6 +21,8 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(1, os.path.abspath('../../'))
 
+import tcms
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -60,10 +62,9 @@ copyright = u'2013, Red Hat, Inc.'
 # built documents.
 #
 # The short X.Y version.
-VERSION = open('../../VERSION.txt', 'r').read().strip('\r\n')
-version = VERSION
+version = tcms.VERSION
 # The full version, including alpha/beta/rc tags.
-release = VERSION
+release = tcms.VERSION
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

tcms/settings/common.py
@@ -2,7 +2,9 @@
 
 import django.conf.global_settings as DEFAULT_SETTINGS
 import os.path
+import tcms
 
+NITRATE_VERSION = tcms.VERSION
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG

Nitrate/Nitrate

docs/source/conf.py
@@ -21,6 +21,8 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(1, os.path.abspath('../../'))
 
+import tcms
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -60,10 +62,9 @@ copyright = u'2013, Red Hat, Inc.'
 # built documents.
 #
 # The short X.Y version.
-VERSION = open('../../VERSION.txt', 'r').read().strip('\r\n')
-version = VERSION
+version = tcms.__version__
 # The full version, including alpha/beta/rc tags.
-release = VERSION
+release = tcms.__version__
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

tcms/settings/common.py
@@ -2,7 +2,9 @@
 
 import django.conf.global_settings as DEFAULT_SETTINGS
 import os.path
+import tcms
 
+NITRATE_VERSION = tcms.__version__
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG