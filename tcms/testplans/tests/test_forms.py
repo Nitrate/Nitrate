8dd1e36fbe3d149a761b6acc80d3039f1af120a2
Chenxiong Qi <qcxhome@gmail.com>
2018-05-25 11:06:58 +0800

    Add missing test in 93ee243
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- tcms/testplans/tests/test_forms.py
+++ tcms/testplans/tests/test_forms.py
@@ -33,4 +33,28 @@ class TestUploadedHTMLFile(unittest.TestCase):
         self.assertEqual('<p>Importing plan</p>', cleaned_content.strip())
 
     def test_necessary_attributes_are_removed(self):
-        pass
+        uploaded_file = Mock()
+        uploaded_file.read.return_value = '''<html>
+    <head>
+        <script type="text/javascript">alert('hello Nitrate')</script>
+        <style type="text/css">p {font-size: 14px}</style>
+        <link rel="icon" href="https://nitrate.com/icon.png" title="Nitrate">
+    </head>
+    <body>
+        <div id="section" class="section">
+            <p style="font-size:large">Importing plan</p>
+        </div>
+        <style type="text/css" media="screen">
+        table {border: 2px}
+        </style>
+        <script type="text/javascript">
+        console.log('testing, testing, ...')
+        </script>
+    </body>
+</html>'''
+        cleaner = UploadedHTMLFile(uploaded_file)
+        cleaned_content = cleaner.get_content()
+        self.assertEqual(
+            '''<div>
+<p>Importing plan</p>
+</div>''', cleaned_content.strip())
