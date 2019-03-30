238815fd870c19fa68fd04b6e4ce498d2065e709
Chenxiong Qi <qcxhome@gmail.com>
2019-03-30 18:06:18 +0800

    Create more fake test plans
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/tcms/core/management/commands/fake_data.py
+++ src/tcms/core/management/commands/fake_data.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.core.management.base import BaseCommand, CommandError
 
 from tcms.management import models as mgt_models
 from tcms.testplans import models as plans_models
 from tcms.profiles import models as profile_models
-
-user = User.objects.get(username='admin')
 
 
 #webapp = mgt_models.Classification.objects.create(
@@ -22,13 +22,45 @@ user = User.objects.get(username='admin')
     #name='Test plan 1',
 #)
 
-bookmarks = (
-    ('name', 'http://localhost/bookmark/')
-)
 
-for i in range(55):
-    profile_models.Bookmark.objects.create(
-        user=user,
-        name=f'name {i}',
-        url=f'http://localhost/bookmark/{i}',
-        description=f'Bookmark for name {i}')
+def create_fake_data():
+    user = User.objects.get(username='admin')
+    site = Site.objects.get_current()
+
+    bookmarks = (
+        ('name', 'http://localhost/bookmark/')
+    )
+
+    for i in range(55):
+        profile_models.Bookmark.objects.create(
+            user=user,
+            name=f'name {i}',
+            url=f'http://localhost/bookmark/{i}',
+            description=f'Bookmark for name {i}',
+            site=site)
+
+    classification, _ = mgt_models.Classification.objects.get_or_create(name='WebApp')
+    product, _ = mgt_models.Product.objects.get_or_create(
+        name='TestProduct', classification=classification)
+    version_1_0, _ = mgt_models.Version.objects.get_or_create(product=product, value='1.0')
+    version_2_0, _ = mgt_models.Version.objects.get_or_create(product=product, value='2.0')
+    plan_type, _ = plans_models.TestPlanType.objects.get_or_create(name='Function')
+
+    for i in range(100):
+        if plans_models.TestPlan.objects.filter(pk=i + 1).exists():
+            continue
+        plans_models.TestPlan.objects.create(
+            name='Test Plan {}'.format(i + 1),
+            product=product,
+            product_version=version_1_0,
+            type=plan_type,
+            author=user)
+
+
+class Command(BaseCommand):
+    help = (
+        'Create fake data only for running Nitrate locally for development purpose.'
+    )
+
+    def handle(self, *args, **options):
+        create_fake_data()
