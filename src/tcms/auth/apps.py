b838732055fd442c14d3cc115981c9aced0db5b9
Chenxiong Qi <qcxhome@gmail.com>
2020-01-31 15:25:56 +0800

    Avoid to add existing email address to user
    Fixes #430
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/tcms/auth/apps.py
+++ src/tcms/auth/apps.py
@@ -1,8 +1,31 @@
 from django.apps import AppConfig as DjangoAppConfig
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+
+
+def user_klass_clean(self):
+    """Add additional validation to avoid using same email address"""
+    from django.contrib.auth.models import User
+
+    # Repeat the real work in User.clean
+    super(User, self).clean()
+    self.email = self.__class__.objects.normalize_email(self.email)
+
+    # Additional checks to validate
+    if self.email and User.objects.filter(email=self.email).exists():
+        raise ValidationError(
+            f'There is already an existing user with email {self.email}')
+
+
+def patch_user_model_clean():
+    from django.contrib.auth.models import User
+    User.clean = user_klass_clean
 
 
 class AppConfig(DjangoAppConfig):
     name = 'tcms.auth'
     label = 'tcms.core.contrib.auth'
     verbose_name = _("Core auth")
+
+    def ready(self):
+        patch_user_model_clean()
