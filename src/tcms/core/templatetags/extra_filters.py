62ffdef87331f2abfa6085650b7a0034a2297088
Chenxiong Qi <qcxhome@gmail.com>
2020-04-14 09:08:46 +0800

    Cleanup template tags
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/tcms/core/templatetags/extra_filters.py
+++ src/tcms/core/templatetags/extra_filters.py
@@ -1,71 +1,17 @@
 # -*- coding: utf-8 -*-
+
+from datetime import timedelta
 from django import template
 
 register = template.Library()
 
 
-@register.filter(name='cutbystring')
-def cut_by_string(value, arg):
-    arg = int(arg)
-    if len(value) < arg:
-        return value
-    else:
-        return value[:arg - 3] + "..."
-
-
-@register.filter(name='ismine')
-def is_mine(object, user):
-    if hasattr(object, 'author') and object.author == user:
-        return True
-    if hasattr(object, 'manager') and object.manager == user:
-        return True
-
-    return False
-
-
-@register.filter(name='smart_unicode')
-def smart_unicode(object):
-    from django.utils.encoding import smart_unicode
-
-    if not object:
-        return object
-    return smart_unicode(object)
-
-
-@register.filter(name='smart_int')
-def smart_int(object):
-    if not object:
-        return object
-
-    try:
-        return int(object)
-    except ValueError:
-        return object
-
-
-@register.filter(name='timedelta2string')
-def timedelta2string(object):
+@register.filter
+def timedelta2string(value: timedelta):
     from tcms.core.utils import format_timedelta
-    return format_timedelta(object)
+    return format_timedelta(value)
 
 
-@register.filter(name='timedelta2seconds')
-def timedelta2seconds(object):
-    return int(object.seconds + object.days * 86400)
-
-
-@register.inclusion_tag('mail/new_run.txt', takes_context=True)
-def absolute_url(context):
-    request = context['request']
-    object = context['test_run']
-    if not hasattr(object, 'get_absolute_url'):
-        return None
-
-    return object.get_absolute_url(request)
-
-
-@register.filter()
-def replace_str(value, arg):
-    """Template filter to replace a substring"""
-    old, new = arg.split(' ', 1)
-    return value.replace(old, new)
+@register.filter
+def timedelta2seconds(value: timedelta):
+    return int(value.seconds + value.days * 86400)
