58d3370aa85a5712cc24713c6eb7b30ba9a85bb9
Chenxiong Qi <qcxhome@gmail.com>
2020-05-05 20:29:53 +0800

    Remove Django 2.0 from project classifiers
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- setup.py
+++ setup.py
@@ -89,7 +89,6 @@ setup(
     zip_safe=False,
     classifiers=[
         'Framework :: Django',
-        'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
