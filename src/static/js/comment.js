86633139ea6e51871761c62cc012def0ad6200bc
Chenxiong Qi <qcxhome@gmail.com>
2020-06-06 16:24:05 +0800

    JS: move comment functions to a separate js file
    Fixes #596
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

---
+++ src/static/js/comment.js
@@ -0,0 +1,50 @@
+/* eslint no-redeclare: "off" */
+/* eslint no-unused-vars: "off" */
+
+function removeComment(form, callback) {
+  let parameters = Nitrate.Utils.formSerialize(form);
+
+  postRequest({
+    url: form.action,
+    data: parameters,
+    success: function (data) {
+      updateCommentsCount(parameters.object_pk, false);
+      callback(data);
+    },
+  });
+}
+
+
+function submitComment(container, parameters, callback) {
+  // FIXME: Remove parameter container, it is not useless here.
+  jQ(container).html('<div class="ajax_loading"></div>');
+
+  postRequest({
+    url: '/comments/post/',
+    data: parameters,
+    success: function () {
+      updateCommentsCount(parameters.object_pk, true);
+      if (callback) {
+        callback();
+      }
+    }
+  });
+}
+
+
+function updateCommentsCount(caseId, increase) {
+  let commentDiv = jQ('#' + caseId + '_case_comment_count');
+  let countText = jQ('#' + caseId + '_comments_count');
+  if (increase) {
+    if (commentDiv.children().length === 1) {
+      commentDiv.prepend('<img src="/static/images/comment.png" style="vertical-align: middle;">');
+    }
+    countText.text(' ' + (parseInt(countText.text()) + 1));
+  } else {
+    if (parseInt(countText.text(), 10) === 1) {
+      commentDiv.html('<span id="' + caseId + '_comments_count"> 0</span>');
+    } else {
+      countText.text(' ' + (parseInt(commentDiv.text()) - 1));
+    }
+  }
+}
