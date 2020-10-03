8b356ed33c622ed852838a70275c923f60f034be
Chenxiong Qi <qcxhome@gmail.com>
2020-10-03 14:06:40 +0800

    JS: fix incorrect data passed to attachment deletion view
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/static/js/deleconfirm.js
+++ src/static/js/deleconfirm.js
@@ -1,13 +1,24 @@
 /* eslint no-redeclare: "off" */
 
-function deleConfirm(attachmentId, home, planId) {
+function deleConfirm(attachmentId, from, objectId) {
   confirmDialog({
     message: 'Are you sure to delete the attachment?',
     title: 'Manage attachments',
     yesFunc: function () {
+      let data = {file_id: attachmentId};
+      switch (from) {
+        case 'from_case':
+          data.from_case = objectId;
+          break;
+        case 'from_plan':
+          data.from_plan = objectId;
+          break;
+        default:
+          throw new Error('Argument home has invalid value "' + from + '"');
+      }
       postRequest({
         url: '/management/deletefile/',
-        data: {file_id: attachmentId, from_plan: planId},
+        data: data,
         success: function () {
           jQ('#' + attachmentId).remove();
           jQ('#attachment_count').text(parseInt(jQ('#attachment_count').text()) - 1);
