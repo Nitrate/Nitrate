22ab7c59267b1ac412574500c2625d3c6e2865aa
Chenxiong Qi <qcxhome@gmail.com>
2021-04-02 22:59:49 +0800

    Fix wrong Template.render call in 500 error handler
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/tcms/core/views/error.py
+++ src/tcms/core/views/error.py
@@ -1,5 +1,5 @@
 from django import http
-from django.template import RequestContext, loader, TemplateDoesNotExist
+from django.template import loader, TemplateDoesNotExist
 from django.views.decorators.csrf import requires_csrf_token
 
 
@@ -18,4 +18,4 @@ def server_error(request, template_name="500.html"):
         template = loader.get_template(template_name)
     except TemplateDoesNotExist:
         return http.HttpResponseServerError("<h1>Server Error (500)</h1>")
-    return http.HttpResponseServerError(template.render(RequestContext(request)))
+    return http.HttpResponseServerError(template.render(request=request))
