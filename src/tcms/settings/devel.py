4499b8bd7602b7b390776f95eaf1480baf98f349
Chenxiong Qi <qcxhome@gmail.com>
2021-04-24 16:40:24 +0800

    Remove django_extensions from devel apps list
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/tcms/settings/devel.py
+++ src/tcms/settings/devel.py
@@ -23,7 +23,6 @@ MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
 INSTALLED_APPS += (
     "debug_toolbar",
-    "django_extensions",
 )
 
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
