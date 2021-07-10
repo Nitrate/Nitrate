fc6408ef0bb06a1f2c0e155067b41ca942e72ebc
Chenxiong Qi <qcxhome@gmail.com>
2021-07-10 15:53:18 +0800

    Use dummy email backend for the worker service
    Set it the same value as the web service has. This ensures the email
    notification works well if no custom real working SMTP server is
    configured.
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- contrib/compose/worker_custom_conf.py
+++ contrib/compose/worker_custom_conf.py
@@ -2,5 +2,4 @@ ASYNC_TASK = 'CELERY'
 CELERY_BROKER_URL = 'amqp://messagebus:5672/myvhost'
 # Celery worker settings
 CELERY_TASK_IGNORE_RESULT = True
-EMAIL_HOST = 'smtp.localhost'
-EMAIL_FROM = 'noreply@example.com'
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
