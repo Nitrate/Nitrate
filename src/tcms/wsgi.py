8ba3aa778b670f31550a6cf79439f0e6fe4317bb
Chenxiong Qi <qcxhome@gmail.com>
2022-03-13 11:44:18 +0800

    bandit: Remove PYTHON_EGG_CACHE from wsgi (#1016)
    bandit detects a hardcoded temporary directory path is used, but the
    environment variable PYTHON_EGG_CACHE is useless for now.
    
    The default httpd conf provided along with this codebase is configured
    to run httpd process in daemon mode. For mod_wsgi, the way of configured
    python-eggs cache directory is different for daemon mode and embedded
    mode. So, embedded mode is required, set WSGIPythonEggs explicitly,
    which can be referred in the mod_wsgi document
    configuration-directives/WSGIPythonEggs.html.
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- src/tcms/wsgi.py
+++ src/tcms/wsgi.py
@@ -22,7 +22,6 @@ import os
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "tcms.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tcms.settings.product")
-os.environ["PYTHON_EGG_CACHE"] = "/tmp/.python-eggs/"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
