b0a460894ff36b54de67cd084d0c5902b660c154
Chenxiong Qi <qcxhome@gmail.com>
2023-05-01 13:26:38 +0800

    Guess testdb_mysql container name
    tox-docker adds suffix to container name to enable the unique container.
    This commit finds out the expected container name by matching regex.
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- contrib/scripts/adjust_mysql_auth_plugin.sh
+++ contrib/scripts/adjust_mysql_auth_plugin.sh
@@ -1,8 +1,16 @@
 set -ex
 username=$1
 password=$2
-c_name=$3
-docker exec $c_name /bin/bash -c "
+tempfile=$(mktemp)
+
+# Guess container name of the test database
+if ! docker ps -a --format "{{ json .Names }}" | tr -d '"' | grep "^testdb_mysql" >"$tempfile" 2>&1; then
+	echo "error: cannot find container testdb_mysql"
+	exit 1
+fi
+c_name=$(cat "$tempfile")
+
+docker exec "$c_name" /bin/bash -c "
 mysql -u${username} -p${password} \
 -e \"ALTER USER '${username}'@'%' IDENTIFIED WITH mysql_native_password BY '${password}'\"
 "
