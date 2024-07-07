1d2e351fd076c92a4ce12dae2eea9be0f7797a27
Chenxiong Qi <qcxhome@gmail.com>
2024-07-07 23:42:52 +0800

    ci: fix tests with real databases
    tox-docker is removed.
    
    The CI process running in GitHub actions depends on database services.
    
    The local tests with real databases runs by the following workflow:
    
    * Run: make start-testdb-{mysql,mariadb,pgsql}
    * Run tests with tox, e.g. tox -e py311-django420-mariadb
    
    Every database container is initialized with most default values except
    the the password is set to "pass".
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- contrib/scripts/adjust_mysql_auth_plugin.sh
+++ contrib/scripts/adjust_mysql_auth_plugin.sh
@@ -1,16 +1,14 @@
 set -ex
+
 username=$1
 password=$2
-tempfile=$(mktemp)
 
-# Guess container name of the test database
-if ! docker ps -a --format "{{ json .Names }}" | tr -d '"' | grep "^testdb_mysql" >"$tempfile" 2>&1; then
-	echo "error: cannot find container testdb_mysql"
-	exit 1
+CONTAINER_NAME=testdb-mysql
+
+if ! docker inspect "$CONTAINER_NAME" >/dev/null 2>&1; then
+    echo "error: cannot find container $CONTAINER_NAME" >&2
+    exit 1
 fi
-c_name=$(cat "$tempfile")
 
-docker exec "$c_name" /bin/bash -c "
-mysql -u${username} -p${password} \
--e \"ALTER USER '${username}'@'%' IDENTIFIED WITH mysql_native_password BY '${password}'\"
-"
+ALTER_USER_CMD="ALTER USER '${username}'@'%' IDENTIFIED WITH mysql_native_password BY '${password}'"
+docker exec "$CONTAINER_NAME" /bin/bash -c "mysql -u${username} -p${password} -e \"${ALTER_USER_CMD}\""
